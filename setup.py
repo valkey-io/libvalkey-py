03089ba4dbcf41875a35788a0d432f8097f871f0
Christian Clauss <cclauss@me.com>
2025-11-06 14:26:02 +0100

    Remove free-threaded trove classifier
    Signed-off-by: Christian Clauss <cclauss@me.com>

--- setup.py
+++ setup.py
@@ -109,7 +109,6 @@ setup(
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: 3.14",
-        "Programming Language :: Python :: Free Threading",
         "Programming Language :: Python :: Implementation :: CPython",
         "Topic :: Software Development",
     ],
